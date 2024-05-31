feefc1035e8713f22e79ac6d78340472fbbb11fb
Juan12331 <juanxadez2@gmail.com>
2024-05-31 09:42:52 -0300

    criando Usuario utilizando sequelize

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 
 
 (async () => {
-    const Usuarios = require('./config/Usuarios')
-    await Usuarios.sync();
+    const Usuarios = require('./config/Usuarios') // chamamos para o sync saber que é essa tabela que precisa ser criada
+    const sequelize = require('./config/sequelize') // esse é o database
+    await sequelize.sync(); //O sync verifica se existe uma tabela Usuarios no banco de dados e se não existir ele cria uma com as informações de usuarios
+
+
+    const novoUsuario = await Usuarios.create({
+        nome: 'juan',
+        email: 'juan1402@gmail.com',
+        senha: 'uncknow-password'
+})
 })();
   
