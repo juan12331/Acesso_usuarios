2ac09a5ca0013ba4d77fbce3595de1f9861a9132
Juan12331 <juanxadez2@gmail.com>
2024-06-25 00:16:57 -0300

    criando pagina de login de administrador e sua respectiva rota

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -2,13 +2,15 @@ import { Route, Routes } from "react-router-dom";
 import Users from "./pages/users/users";
 import Create from "./pages/create_users/create";
 import Update from "./pages/update_users/update";
+import Login from "./pages/login_adm/login"
 
 
 
 function MainRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Users />} />
+      <Route path="/" element={<Login />} />
+      <Route path="/usuarios" element={<Users />} />
       <Route path="/criar_usuario" element={<Create/>} />
       <Route path="/atualizar_usuario" element={<Update/>}/>      
     </Routes>
