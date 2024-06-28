8bda4433ea347eaac89fb4d8d3117d8b0888567e
Juan12331 <juanxadez2@gmail.com>
2024-06-28 01:56:49 -0300

    fazendo tela de login de adms (update no css)

--- frontend/src/pages/login_adm/login.jsx
+++ frontend/src/pages/login_adm/login.jsx
@@ -27,12 +27,24 @@ const login = () => {
     }
 
   return (
-    <div>
-        <input type="text" id='email' className='email' value={email} onChange={(e) => setEmail(e.target.value)}/>
-        <input type="password" id='password' className='password' value={password} onChange={(e) => setpassword(e.target.value)} />
-        <button onClick={() =>autenticacao(email, password)}> Logar</button>
+    
+    <div className="alinhar">
+    <div className='conteiner'>
+        <h1 className='h1'>Login de usuario</h1>
+        <span className='span'>coloque seu email e sua senha abaixo</span>
+        <input type="text" id='email' className='email input' value={email} onChange={(e) => setEmail(e.target.value)}/>
+        <input type="password" id='password' className='password input' value={password} onChange={(e) => setpassword(e.target.value)} />
+        <span className='span'></span>
+        <button onClick={() =>autenticacao(email, password)} className='button'> Logar</button>
+        
     </div>
+    </div>
+
+  
+    
   )
 }
 
 export default login
+
+//https://static.vecteezy.com/ti/vetor-gratis/p1/1849553-fundo-ouro-moderno-vetor.jpg
